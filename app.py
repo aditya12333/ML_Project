f464ac85761927d9a88c0037e87f7735539100cf
Aditya Pandey <aditya.pandey@cognida.ai>
2023-11-12 14:30:26 +0530

    model training and evaluation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainingConfig, ModelTrainer
 
 if __name__=="__main__":
     logging.info("The execution has started")
@@ -14,7 +15,12 @@ if __name__=="__main__":
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr, test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## model training
+        #model_training_config = ModelTrainingConfig()
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
        
         
